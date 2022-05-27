d3b4783c4fc8c7dae17ed5e2338b37093d9263d1
unknown <matt.mcquain@gmail.com>
2022-05-26 23:16:45 -0700

    created all folders and files

--- src/App.js
+++ src/App.js
@@ -52,29 +52,29 @@ const App = () => {
           {/* Routing */}
           <Routes>
           {/* Dashboard */}
-            <Route path='/' element='Ecommerce' />
-            <Route path='/ecommerce' element='Ecommerce' />
+            <Route path='/' element={<Ecommerce /> } />
+            <Route path='/ecommerce' element={<Ecommerce /> } />
 
             {/* Pages */}
-            <Route path='/orders' element='Orders' />
-            <Route path='/employees' element='Employees' />
-            <Route path='/customers' element='Customers' />
+            <Route path='/orders' element={<Orders /> } />
+            <Route path='/employees' element={<Employees /> } />
+            <Route path='/customers' element={<Customers /> } />
 
             {/* Apps */}
-            <Route path='/kanban' element='Kanban' />
-            <Route path='/editor' element='Editor' />
-            <Route path='/calendar' element='Calendar' />
-            <Route path='/color-picker' element='ColorPicker' />
+            <Route path='/kanban' element={<Kanban /> } />
+            <Route path='/editor' element={<Editor /> } />
+            <Route path='/calendar' element={<Calendar /> } />
+            <Route path='/color-picker' element={<ColorPicker /> } />
 
             {/* Charts */}
-            <Route path='/line' element='Line' />
-            <Route path='/area' element='Area' />
-            <Route path='/bar' element='Bar' />
-            <Route path='/pie' element='Pie' />
-            <Route path='/financial' element='Financial' />
-            <Route path='/color-mapping' element='ColorMapping' />
-            <Route path='/pyramid' element='Pyramid' />
-            <Route path='/stacked' element='Stacked' />
+            <Route path='/line' element={<Line /> } />
+            <Route path='/area' element={<Area /> } />
+            <Route path='/bar' element={<Bar /> } />
+            <Route path='/pie' element={<Pie /> } />
+            <Route path='/financial' element={<Financial /> } />
+            <Route path='/color-mapping' element={<ColorMapping /> } />
+            <Route path='/pyramid' element={<Pyramid /> } />
+            <Route path='/stacked' element={<Stacked /> } />
           </Routes>
         </div>
       </BrowserRouter>
